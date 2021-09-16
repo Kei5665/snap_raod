220049194c3430f37814508f96555d84e5eff683
Kei <kt10195665@yahoo.ne.jp>
2021-09-17 08:57:09 +0900

    modified:初期設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module SnapRaod
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.skip_routes true # ルーティングを追加しない
+      g.assets false # アセットファイルを生成しない
+      g.test_framework false # テストを生成しない
+      g.helper false # ヘルパーファイルを生成
+    end   
+    # 国際化
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s] 
+    # タイムゾーン
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
