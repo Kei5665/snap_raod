d7ccdb6e6f8c9a2971eb8def983e9cdc6e46ab2a
Kei <kt10195665@yahoo.ne.jp>
2021-09-17 09:40:50 +0900

    modified:createアクションを作成

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,24 @@
 class MapsController < ApplicationController
+  protect_from_forgery
   def index
   end
 
   def top
+  end
+
+  def create 
+    json =  params[:data]
+    title = params[:title]
+    name = params[:name]
+    body = params[:body]
+    json_data = JSON.parse(json)
+
+    @json = Map.new(
+        jsonb_data: json_data,
+        title: title,
+        name: name,
+        body: body
+    )
+    @json.save!
   end
 end
