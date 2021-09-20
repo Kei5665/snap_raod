11a06d2be4550783a3fb2f86bee8b33e32344716
Kei <kt10195665@yahoo.ne.jp>
2021-09-21 02:28:54 +0900

    fix:trialのルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  root 'welcome_pages#top'
-
   resources :maps
-  resources :trials
   get '/maps_all',to: 'maps#top'
-  get '/trials_all',to: 'trials#top'
-  get 'select', to: 'welcome_pages#index'
 end
